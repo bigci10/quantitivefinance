6c892b25dba4c818acd0d520234e47dde3662e5f
Bilal Igci <70436168+bigci10@users.noreply.github.com>
2025-03-10 21:23:17 +0100

    Create ideas.py

---
+++ ideas.py
@@ -0,0 +1,7 @@
+"""
+  Center of mass
+  1*In physics, the center of mass is found by weighting positions with mass. 
+  Similarly, in finance, we can determine the center of price movement by weighting price changes with volume
+  :PriceCenter = ∑(PriceChange * Volume) / ∑(Volume)
+                   
+"""
